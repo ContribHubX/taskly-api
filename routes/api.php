21b871de153bd6e234c3ba0e20f3590246c7f06d
Yander <lorenzolubguban@gmail.com>
2025-03-23 12:38:45 +0800

    feat: apply authentication middleware to protected routes

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 // Protected Authentication Routes (Require Authentication)
-Route::group(function () {
+Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'getMyDetails']);
 
@@ -23,7 +23,7 @@ Route::group(function () {
         Route::post('/forgot', [AuthController::class, 'sendResetLink']);
         Route::post('/reset', [AuthController::class, 'resetPassword'])->name('password.update');
     });
-})->middleware('auth:sanctum');
+});
 
 
 
